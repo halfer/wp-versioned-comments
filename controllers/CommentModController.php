7933d93a3c306bd2d8769ae0cffd699d1a80aace
Jon Hinks <projects.ws@jondh.me.uk>
2013-07-06 18:56:29 +0100

    Sketch out skeleton of comment version history system

--- controllers/CommentModController.php
+++ controllers/CommentModController.php
@@ -2,8 +2,29 @@
 
 class CommentModController extends TemplateSystem
 {
+	const PATH_PLUGIN_NAME = 'wp-comment-mod';
+
 	public function execute()
 	{
-		
+		$this->initCss();
+		add_action( 'add_meta_boxes_comment', array($this, 'moderatorOptions') );
+	}
+
+	protected function initCss()
+	{
+		// Get plugins folder relative to wp root
+		$site = site_url();
+		$relativePath = substr(plugins_url(), strlen($site));
+
+		wp_register_style(
+			'commentmod_css',
+			$relativePath . '/' . self::PATH_PLUGIN_NAME . '/styles/main.css'
+		);
+		wp_enqueue_style('commentmod_css');		
+	}
+
+	public function moderatorOptions()
+	{
+		$this->renderTemplate('version-history');
 	}
 }
---
+++ templates/version-history.php
@@ -0,0 +1,22 @@
+<div id="comment-version-history" class="stuffbox">
+	<h3>
+		<label>Version history</label>
+	</h3>
+	<div class="inside">
+		<div>
+			<input
+				type="checkbox" id="save_version" name="save_version" checked="checked"
+			/>
+			<label for="save_version">Create previous version prior to save</label>
+		</div>
+
+		<fieldset>
+			<legend>Version 2 (6 July 2013 18:49)</legend>
+			 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fringilla dignissim rhoncus. Vestibulum eu libero porta, semper velit id, ullamcorper nibh. Nam lorem turpis, elementum eget porttitor id, posuere id mauris. Sed pretium scelerisque scelerisque. Ut ornare elit in libero mattis, euismod sagittis lacus accumsan. Aenean quis mauris a magna vestibulum aliquet a venenatis magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris faucibus augue a magna mollis sollicitudin. Duis vel sollicitudin arcu. Pellentesque bibendum id orci et porta. Quisque placerat egestas quam, vel imperdiet quam vehicula non. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam in iaculis lorem. Suspendisse tincidunt molestie libero, et blandit ante hendrerit interdum. Nunc in leo fringilla, luctus lectus a, lacinia mi. 
+		</fieldset>
+		<fieldset>
+			<legend>Version 1 (5 July 2013 17:12)</legend>
+			 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fringilla dignissim rhoncus. Vestibulum eu libero porta, semper velit id, ullamcorper nibh. Nam lorem turpis, elementum eget porttitor id, posuere id mauris. Sed pretium scelerisque scelerisque. Ut ornare elit in libero mattis, euismod sagittis lacus accumsan. Aenean quis mauris a magna vestibulum aliquet a venenatis magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris faucibus augue a magna mollis sollicitudin. Duis vel sollicitudin arcu. Pellentesque bibendum id orci et porta. Quisque placerat egestas quam, vel imperdiet quam vehicula non. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam in iaculis lorem. Suspendisse tincidunt molestie libero, et blandit ante hendrerit interdum. Nunc in leo fringilla, luctus lectus a, lacinia mi. 
+		</fieldset>
+	</div>
+</div>
